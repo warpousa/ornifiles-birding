b0a56031c921116cfac8457a297aed79fac51201
Corey Jordan-Rutledge <warpousa@warpomedia.com>
2024-01-10 19:23:40 -0500

    Update orni-script.js -- Added randomize class feature
    Randomized classes for banner to support random images; also changed 'var' to 'let'

--- js/orni-script.js
+++ js/orni-script.js
@@ -1,15 +1,24 @@
-/* Start Ornifiles Javascript Customizations - When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* Start Ornifiles Javascript Customizations */ 
+
+// Assign random classes to specifc element(s) //
+let classes = new Array ('banner1', 'banner2', 'banner3');
+let length  = classes.length;
+let bannerImage = jQuery('.banner');
+
+jQuery.each( bannerImage, function(key, value) {
+	jQuery(value).addClass( classes[ Math.floor ( Math.random() * length ) ] );
+});
+
+// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar //
 
 document.getElementById("header").classList.add("show-header");
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
+	let currentScrollPos = window.pageYOffset;
 	if (prevScrollpos > currentScrollPos) {
-		// document.getElementById("header").style.top = "0";
 		document.getElementById("header").classList.add("show-header");
 		document.getElementById("header").classList.remove("hide-header");
 	} else {
-		// document.getElementById("header").style.top = "-80px";
 		document.getElementById("header").classList.add("hide-header");
 		document.getElementById("header").classList.remove("show-header");
 	}
